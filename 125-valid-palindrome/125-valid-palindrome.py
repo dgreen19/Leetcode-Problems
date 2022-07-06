c51d6f595576e6164dfb3f6d0b6c3bab76d401c7
Darryl <green.jr.darryl@gmail.com>
2022-07-06 17:43:56 -0400

    Time: 90 ms (27.88%), Space: 14.5 MB (57.63%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,23 +1,11 @@
-"""Technically more efficient solution due to using less memory"""
+"""Solution using inbuilt functions, debateably not efficient due to using extra memory"""
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        l, r = 0, len(s) - 1
-        
-        while l < r:
-            while l < r and not self.alphaNum(s[l]):
-                l += 1
-            while r > l and not self.alphaNum(s[r]):
-                r -= 1
+        string_value = ''
+        for x in s:
+            if x.isalnum():
+                string_value += x.lower()
                 
-            if s[l].lower() != s[r].lower():
-                return False
-            l, r = l + 1, r - 1
-        return True
+        return string_value == string_value[::-1]
         
-        
-    def alphaNum(self, c):
-        return (ord('A') <= ord(c) <= ord('Z') or
-                ord('a') <= ord(c) <= ord('z') or
-                ord('0') <= ord(c) <= ord('9'))
-      
